967d41764f06dd7f95c13746023b7221fa2a787b
YueWang <617816120@qq.com>
2023-06-18 11:32:25 +0100

    Add module3 display

--- EduCity/learning_scene.gd
+++ EduCity/learning_scene.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var backButton = Button.new()
+var closeButton = Button.new()
 
 var aiButton = Button.new()
 var securityButton = Button.new()
@@ -10,14 +11,19 @@ var automationButton = Button.new()
 
 var lesson1Button = Button.new()
 var lesson2Button = Button.new()
+var lesson3Button = Button.new()
 var quiz1Button = Button.new()
 var quiz2Button = Button.new()
+var quiz3Button = Button.new()
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	backButton = get_node("BackButton")
 	backButton.connect("pressed", _on_backButton_pressed)
+	
+	closeButton = get_node("PopupMenu/CloseButton")
+	closeButton.connect("pressed", _on_closeButton_pressed)
 	
 	aiButton = get_node("AI/AIButton")
 	aiButton.connect("pressed", _on_aiButton_pressed)
@@ -40,11 +46,19 @@ func _ready():
 	lesson2Button = get_node("PopupMenu/Lesson2Button")
 	lesson2Button.connect("pressed", _on_lesson2Button_pressed)
 	
+	lesson3Button = get_node("PopupMenu/Lesson3Button")
+	lesson3Button.connect("pressed", _on_lesson3Button_pressed)
+	lesson3Button.visible = false
+	
 	quiz1Button = get_node("PopupMenu/Quiz1Button")
 	quiz1Button.connect("pressed", _on_quiz1Button_pressed)
 	
-	quiz1Button = get_node("PopupMenu/Quiz2Button")
-	quiz1Button.connect("pressed", _on_quiz2Button_pressed)
+	quiz2Button = get_node("PopupMenu/Quiz2Button")
+	quiz2Button.connect("pressed", _on_quiz2Button_pressed)
+	
+	quiz3Button = get_node("PopupMenu/Quiz3Button")
+	quiz3Button.connect("pressed", _on_quiz3Button_pressed)
+	quiz3Button.visible = false
 	
 	
 # generate lesson/quiz path
@@ -58,6 +72,11 @@ func _on_lesson2Button_pressed():
 	get_tree().change_scene_to_file("res://reading_scene.tscn")
 	
 
+func _on_lesson3Button_pressed():
+	GameManager.reading_path = GameManager.reading_path + "/module3/reading.md"
+	get_tree().change_scene_to_file("res://reading_scene.tscn")
+	
+
 func _on_quiz1Button_pressed():
 	GameManager.question_path = GameManager.question_path + "/module1/quiz.json"
 	get_tree().change_scene_to_file("res://quiz_scene.tscn")
@@ -67,11 +86,16 @@ func _on_quiz2Button_pressed():
 	GameManager.question_path = "/module2/quiz.json"
 	get_tree().change_scene_to_file("res://quiz_scene.tscn")
 	
+	
+func _on_quiz3Button_pressed():
+	GameManager.question_path = "/module3/quiz.json"
+	get_tree().change_scene_to_file("res://quiz_scene.tscn")
+
 
 func _on_backButton_pressed():
 	get_tree().change_scene_to_file("res://main_scene.tscn")
-
-
+	
+	
 # generate module path
 func _on_aiButton_pressed():
 	GameManager.reading_path = "res://LearningResources/IBM AI"
@@ -79,6 +103,8 @@ func _on_aiButton_pressed():
 	
 	var popup = get_node("PopupMenu")
 	popup.visible = true
+	
+	_show_buttons()
 
 
 func _on_securityButton_pressed():
@@ -88,6 +114,8 @@ func _on_securityButton_pressed():
 	var popup = get_node("PopupMenu")
 	popup.visible = true
 	
+	_show_buttons()
+	
 	
 func _on_cloudButton_pressed():
 	GameManager.reading_path = "res://LearningResources/IBM Cloud"
@@ -95,6 +123,8 @@ func _on_cloudButton_pressed():
 	
 	var popup = get_node("PopupMenu")
 	popup.visible = true
+	
+	_show_buttons()
 	
 	
 func _on_dsButton_pressed():
@@ -104,6 +134,8 @@ func _on_dsButton_pressed():
 	var popup = get_node("PopupMenu")
 	popup.visible = true
 	
+	_show_buttons()
+	
 	
 func _on_automationButton_pressed():
 	GameManager.reading_path = "res://LearningResources/IBM Automation"
@@ -112,6 +144,21 @@ func _on_automationButton_pressed():
 	var popup = get_node("PopupMenu")
 	popup.visible = true
 	
+	_show_buttons()
+	
+	
+func _on_closeButton_pressed():
+	lesson3Button.visible = false
+	quiz3Button.visible = false
+	get_node("PopupMenu").visible = false
+	
+	
+func _show_buttons():
+	var module3_path = GameManager.reading_path + "/module3/reading.md"
+	if FileAccess.file_exists(module3_path):
+		lesson3Button.visible = true
+		quiz3Button.visible = true
+
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
