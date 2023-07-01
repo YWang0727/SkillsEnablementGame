241699c3eeb8e5bf5ec32d9adbdedf23d9fef5be
YueWang <617816120@qq.com>
2023-07-01 14:45:13 +0100

    update database generator

---
+++ Game Plan/script7.1.sql
@@ -0,0 +1,53 @@
+create table citymap
+(
+    id                bigint      not null
+        primary key,
+    name              varchar(50) not null,
+    prosperity        bigint      not null,
+    gold              bigint      not null,
+    constructionSpeed int         not null,
+    constraint CityMap_id_uindex
+        unique (id)
+);
+
+create table takenmapcell
+(
+    mapId      bigint not null,
+    positionX  int    not null,
+    positionY  int    not null,
+    houseType  int    not null,
+    houseLevel int    not null,
+    primary key (mapId, positionX, positionY),
+    constraint TakenMapCell_mapId_uindex
+        unique (mapId)
+);
+
+create table user
+(
+    id         bigint auto_increment
+        primary key,
+    email      varchar(256) not null,
+    password   varchar(256) not null,
+    avatar     mediumblob   null,
+    name       varchar(50)  not null,
+    cityMap    bigint       not null,
+    isActive   tinyint      not null comment '0-not actived 1-actived',
+    activeCode varchar(50)  null,
+    constraint User_cityMap_uindex
+        unique (cityMap),
+    constraint User_email_uindex
+        unique (email),
+    constraint User_id_uindex
+        unique (id)
+);
+
+create table user_quiz
+(
+    id       bigint not null,
+    quizId   int    not null,
+    topScore int    null,
+    attempts int    null,
+    primary key (id, quizId)
+);
+
+
