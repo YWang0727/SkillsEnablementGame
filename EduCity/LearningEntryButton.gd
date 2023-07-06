aa169aa30be73a1b3aeb9c976d968031747ff139
YueWang <617816120@qq.com>
2023-07-06 11:47:05 +0100

    revert

--- EduCity/LearningEntryButton.gd
+++ EduCity/LearningEntryButton.gd
@@ -1,15 +1,14 @@
-extends Label
+extends Control
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	self.connect("pressed", _on_Button_pressed)
 
 
+func _on_Button_pressed():
+	get_tree().change_scene_to_file("res://LearningPage/learning_scene.tscn")
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	text = "Prosperity    :  " + String.num_int64(Num.prosperity)
 	pass
-
-
-
