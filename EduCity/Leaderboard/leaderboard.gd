7c95929b9367b690c18aac749f19fb6700bd37b5
lihanyu7 <go22282@bristol.ac.uk>
2023-07-06 11:57:50 +0100

    components

---
+++ EduCity/Leaderboard/leaderboard.gd
@@ -0,0 +1,12 @@
+extends Node
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _on_button_pressed():
+	queue_free()
+	
+
+
