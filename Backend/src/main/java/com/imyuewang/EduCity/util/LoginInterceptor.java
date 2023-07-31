b283c5f2923e4ae837dbc3ef37e3318c8723b9d5
Sarah <cheungsiru1111@gmail.com>
2023-07-31 18:25:33 +0100

    exclude options request

--- Backend/src/main/java/com/imyuewang/EduCity/util/LoginInterceptor.java
+++ Backend/src/main/java/com/imyuewang/EduCity/util/LoginInterceptor.java
@@ -22,7 +22,10 @@ import java.io.PrintWriter;
 public class LoginInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            return true;
+        }
+        
         System.out.println(request.getRequestURL());
         System.out.println(request.getHeader("Authorization"));
         String token = request.getHeader("Authorization");
